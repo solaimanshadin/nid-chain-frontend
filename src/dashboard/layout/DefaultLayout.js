bbcb2646fb4a7f97fa5fc24fe7ae7b768ff31850
Solaiman Shadin <solaimanshadin@gmail.com>
2022-05-25 02:59:02 +0600

    [ADD] NID hook

--- src/dashboard/layout/DefaultLayout.js
+++ src/dashboard/layout/DefaultLayout.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
 import 'dashboard/scss/style.scss'
+import Swal from 'sweetalert2';
+
 const DefaultLayout = () => {
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        Swal.fire({
+          icon: "error",
+          title: "Blockchain wallet not detected!",
+          text: 'You need to connect your blockchain wallet to use this application. Use meta "Metamask" and "Rinkeby" network'
+        })
+        return;
+      }
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+
+      const rinkebyChainID = "0x4";
+      if (chainId !== rinkebyChainID) {
+        Swal.fire({
+          icon: "error",
+          title: "Wrong network!",
+          text: 'You are connected to wrong network, to use this application connect to "Rinkeby" network'
+        })
+        return;
+      }
+      await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Browser not compatible!",
+        text: 'To connect with blockchain network you will need to connect "Metamask" wallet in rinkeby network'
+      })
+      console.log("Filed to connect!")
+    }
+  };
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
   return (
     <div>
       <AppSidebar />
